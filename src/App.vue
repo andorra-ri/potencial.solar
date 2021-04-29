46480f6f8f1ef2ed8bb285c1bf503e1c53136d51
mvilella <mvilella@obsa.ad>
2021-04-29 12:07:31 +0200

    Load roofs layer
    ROOFS layer is not included in the git repo to prevent data leak

--- src/App.vue
+++ src/App.vue
@@ -3,22 +3,40 @@
 </template>
 
 <script>
-import { onMounted } from 'vue';
-import { useMap, useControls } from 'mapbox-composition';
+import { ref, onMounted } from 'vue';
+import { useMap, useControls, useGeoJSON, usePopup } from 'mapbox-composition';
 import config from './config.yaml';
 
+import ROOFS from './assets/roofs.json';
 const { VITE_MAPBOX_TOKEN: accessToken } = import.meta.env;
 
 export default {
 	name: 'App',
 	setup() {
+		const activeRoof = ref(undefined);
+
 		onMounted(async () => {
 			const map = await useMap('map', { ...config.map, accessToken });
 			const { addNavigation } = useControls(map);
 			addNavigation();
+
+			const popup = usePopup(map, {
+				name: 'roof-popup',
+				closeOnClick: false,
+			});
+
+			useGeoJSON(map, {
+				name: 'roofs',
+				source: ROOFS,
+				layers: config.layers,
+				onClick: ({ lngLat, features }) => {
+					popup.setLocation(lngLat);
+					activeRoof.value = features[0].properties;
+				},
+			});
 		});
 
-		return {};
+		return { activeRoof };
 	},
 };
 </script>
