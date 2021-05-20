8134c8a0f4d7a1ad0e97b68fe125bb6e464a5bc3
mvilella <mvilella@obsa.ad>
2021-05-20 19:37:13 +0200

    Clean Rooftop popup
    Rooftop popup only contains information about the solar resource and the installation

--- src/components/RoofPopup.vue
+++ src/components/RoofPopup.vue
@@ -1,40 +1,12 @@
 <template>
 	<teleport :to="`#${to}`">
 		<h2>Edifici CESI {{ roof.cesi }}</h2>
-		<details open>
-			<summary>Recurs solar</summary>
+		<details v-for="(metrics, group) in { resource, installation }" :key="group" open>
+			<summary>{{ t(`metric.${group}`) }}</summary>
 			<ul class="metrics">
-				<li v-for="metric in resource" :key="metric.name">
-					<em>{{ metric.name }}</em>
-					<strong>{{ metric.value }} <small>{{ metric.unit }}</small></strong>
-				</li>
-			</ul>
-		</details>
-		<details open>
-			<summary>Instal·lació</summary>
-			<ul class="metrics">
-				<li v-for="metric in installation" :key="metric.name">
-					<em>{{ metric.name }}</em>
-					<strong>{{ metric.value }} <small>{{ metric.unit }}</small></strong>
-				</li>
-			</ul>
-		</details>
-		<details open>
-			<summary>Autoconsum {{ selfSupply * 100 }}%</summary>
-			<input
-				v-model="selfSupply"
-				type="range"
-				min="0"
-				max="1"
-				step="0.1">
-			<p class="">{{ selfEnergy }} <small>MWh/any</small></p>
-		</details>
-		<details open>
-			<summary>Econòmic</summary>
-			<ul class="metrics">
-				<li v-for="metric in economics" :key="metric.name">
-					<em>{{ metric.name }}</em>
-					<strong>{{ metric.value }} <small>{{ metric.unit }}</small></strong>
+				<li v-for="(metric, name) in metrics" :key="name">
+					<em>{{ t(`metric.${name}.label`) }}</em>
+					<strong>{{ metric }} <small>{{ t(`metric.${name}.unit`, ' ') }}</small></strong>
 				</li>
 			</ul>
 		</details>
@@ -42,12 +14,9 @@
 </template>
 
 <script>
-import { ref, computed } from 'vue';
+import { computed } from 'vue';
+import { useI18n } from 'vue-i18n';
 import { useFormat } from '/@/utils/index';
-import { constants } from '/@/config.yaml';
-
-const { COSTS, ENERGY_PRICE, ENERGY_INJECT_PRICE } = constants;
-const findCost = (costs, power) => costs.find(([limit]) => power <= limit)[1];
 
 export default {
 	name: 'RoofPopup',
@@ -56,43 +25,19 @@ export default {
 		roof: { type: Object, required: true },
 	},
 	setup(props) {
+		const { t } = useI18n();
 		const { number } = useFormat('ca');
-		const selfSupply = ref(0.5);
 
-		const resource = computed(() => [
-			{ name: 'Irradiació', value: number(props.roof.mean_rad, 0), unit: 'Wh/m2' },
-		]);
+		const resource = computed(() => ({ radiation: number(props.roof.mean_rad, 0) }));
 
-		const installation = computed(() => [
-			{
-				name: 'Àrea útil / total',
-				value: `${number(props.roof.use_area, 0)} / ${number(props.roof.area, 0)}`,
-				unit: 'm2',
-			},
-			{ name: 'Mòduls', value: number(props.roof.panels) },
-			{ name: 'Potència', value: number(props.roof.power, 2), unit: 'kWp' },
-			{ name: 'Energia', value: number(props.roof.energy, 2), unit: 'MWh/any' },
-		]);
+		const installation = computed(() => ({
+			area: `${number(props.roof.use_area, 0)} / ${number(props.roof.area, 0)}`,
+			panels: number(props.roof.panels),
+			power: number(props.roof.power, 2),
+			energy: number(props.roof.energy, 2),
+		}));
 
-		// Costs
-		const economics = computed(() => {
-			const installCost = findCost(COSTS.INSTALL, props.roof.power) * props.roof.power * 1000;
-			const operationCost = findCost(COSTS.OPERATION, props.roof.power) * props.roof.power;
-			const savings = props.roof.energy * selfSupply.value * ENERGY_PRICE * 1000;
-			const earning = props.roof.energy * (1 - selfSupply.value) * ENERGY_INJECT_PRICE * 1000;
-			const returnPeriod = installCost / (savings + earning - operationCost);
-			return [
-				{ name: 'Inversió', value: number(installCost), unit: '€' },
-				{ name: 'Cost operació', value: number(operationCost), unit: '€ / any' },
-				{ name: 'Estalvi autoconsum', value: number(savings), unit: '€ / any' },
-				{ name: "Guany d'injecció", value: number(earning), unit: '€ / any' },
-				{ name: 'Període retorn', value: number(returnPeriod, 1), unit: 'anys' },
-			];
-		});
-
-		const selfEnergy = computed(() => number(selfSupply.value * props.roof.energy));
-
-		return { resource, installation, economics, selfSupply, selfEnergy };
+		return { t, resource, installation };
 	},
 };
 </script>
