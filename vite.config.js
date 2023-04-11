291c5e882a843b70094bbb6805122a5c8f799176
Marc Vilella <mvilella.and@gmail.com>
2023-04-11 18:12:40 +0200

    chore: Install TypeScript

--- vite.config.js
+++ vite.config.js
@@ -7,18 +7,18 @@ import autoprefixer from 'autoprefixer';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [
-		vue(),
-		yaml(),
-		vueI18n({
-			compositionOnly: true,
-			include: [path.resolve(__dirname, './src/locales/**')],
-		}),
-	],
-	css: { postcss: { plugins: [autoprefixer()] } },
-	resolve: {
-		alias: [
-			{ find: '/@', replacement: path.resolve(__dirname, './src') },
-		],
-	},
+  plugins: [
+    vue(),
+    yaml(),
+    vueI18n({
+      compositionOnly: true,
+      include: [path.resolve(__dirname, './src/locales/**')],
+    }),
+  ],
+  css: { postcss: { plugins: [autoprefixer()] } },
+  resolve: {
+    alias: [
+      { find: '/@', replacement: path.resolve(__dirname, './src') },
+    ],
+  },
 });
